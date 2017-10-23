4dbb5af20227f27a79f81e20a73d51aa4414ebd4
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-23 22:09:19 +0000

    Mon Oct 23 23:09:19 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,3 +1,27 @@
 # noinspection PyUnusedLocal
+letter_list = ["A","B","C","D"]
+def illegal_input(item_list):
+	for it in letter_list:
+		item_list = item_list.replace(it, "")
+
+	if len(item_list) != 0:
+		return True
+	else:
+		return False
+
 def checkout(skus):
-    raise NotImplementedError()
+    if illegal_input(skus) == True:
+    	return -1
+
+    total_price = 0
+    sku_list = list(skus)
+    a_price = 50
+    b_price = 30
+    c_price = 20
+    d_price = 15
+
+    
+if __name__ == '__main__':
+    checkout("A")
+    checkout("A")
+    checkout("A")
