f282c3c5028337b5c322aff90709c4b737ee0166
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-23 21:57:48 +0000

    Mon Oct 23 22:57:48 BST 2017

--- lib/solutions/hello.py
+++ lib/solutions/hello.py
@@ -1,8 +1,3 @@
 # noinspection PyUnusedLocal
 def hello(friend_name):
-	print isinstance(friend_name, str)
-	if isinstance(friend_name, str) == False:
-		print friend_name
-		return
-
-	print "Hello, World!"
+	return "Hello, World!"
