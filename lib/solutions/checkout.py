ab3ebb9481186b4f2a568583c44fe2b16df89591
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-23 22:39:39 +0000

    Mon Oct 23 23:39:39 BST 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -10,25 +10,38 @@ def illegal_input(item_list):
 		return False
 
 def checkout(skus):
-    if illegal_input(skus) == True:
-    	return -1
+	if illegal_input(skus) == True:
+		return -1
 
-    total_price = 0
-    sku_list = list(skus)
-    a_price = 50
-    b_price = 30
-    c_price = 20
-    d_price = 15
+	total_price = 0
+	sku_list = list(skus)
+	a_price = 50
+	b_price = 30
+	c_price = 20
+	d_price = 15
+	e_price = 40
 
-    total_price = total_price + a_price * skus.count("A")
-    total_price = total_price + b_price * skus.count("B")
-    total_price = total_price + c_price * skus.count("C")
-    total_price = total_price + d_price * skus.count("D")
+	b_count = skus.count("B")
 
-    if skus.count("A") >= 3:
-    	total_price -= 20 * int(skus.count("A") / 3)
+	five_a_price = (a_price * int(skus.count("A") / 5) * 5 - int(skus.count("A") / 5) * 50)
+	left_a_count = skus.count("A") - int(skus.count("A") / 5) * 5
+	three_a_price = (a_price * int(left_a_count / 3) * 3 - int(left_a_count / 3) * 20)
+	double_left_a_count = left_a_count - int(left_a_count / 3) * 3
+	total_price =  five_a_price + three_a_price + a_price * double_left_a_count
 
-    if skus.count("B") >= 2:
-    	total_price -= 15 * int(skus.count("B") / 2)
+	b_count += skus.count("E") / 2
+	real_b_count = 0
+	if skus.count("B") > 0:
+		if b_count > skus.count("B"):
+			real_b_count = 0
+		else:
+			real_b_count = skus.count("B") - b_count
+	else:
+		real_b_count = 0
 
-    return total_price
+	total_price = total_price + b_price * real_b_count - 15 * int(real_b_count / 2)
+	total_price = total_price + c_price * skus.count("C")
+	total_price = total_price + d_price * skus.count("D")
+	total_price = total_price + e_price * skus.count("E")
+
+	return total_price
